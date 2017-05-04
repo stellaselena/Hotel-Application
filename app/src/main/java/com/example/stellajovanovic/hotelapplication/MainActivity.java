e68d936cc5c6272db2a95e71660ec7a4ad07b935
stellaselena <jovste15@student.westerdals.no>
2017-05-04 18:59:38 +0200

    added main menu icons

--- app/src/main/java/com/example/stellajovanovic/hotelapplication/MainActivity.java
+++ app/src/main/java/com/example/stellajovanovic/hotelapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.stellajovanovic.hotelapplication;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
