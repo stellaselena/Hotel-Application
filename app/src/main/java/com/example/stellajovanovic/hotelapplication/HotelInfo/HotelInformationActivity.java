a6fdf8743b68d27f14f08f8cd881ad81b25a8cba
stellaselena <jovste15@student.westerdals.no>
2017-05-06 14:29:38 +0200

    added webview in outandabout

--- app/src/main/java/com/example/stellajovanovic/hotelapplication/HotelInfo/HotelInformationActivity.java
+++ app/src/main/java/com/example/stellajovanovic/hotelapplication/HotelInfo/HotelInformationActivity.java
@@ -22,23 +22,11 @@ public class HotelInformationActivity extends FragmentActivity implements OnMapR
         setContentView(R.layout.activity_hotel_information);
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
--- app/src/main/java/com/example/stellajovanovic/hotelapplication/OutAndABout/OutAndAboutActivity.java
+++ app/src/main/java/com/example/stellajovanovic/hotelapplication/OutAndABout/OutAndAboutActivity.java
@@ -2,15 +2,37 @@ package com.example.stellajovanovic.hotelapplication.OutAndABout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.stellajovanovic.hotelapplication.R;
 
 public class OutAndAboutActivity extends AppCompatActivity {
+    WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_out_and_about);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        web = (WebView) findViewById(R.id.webView1);
+
+        WebSettings wbset = web.getSettings();
+        wbset.setJavaScriptEnabled(true);
+        web.setWebViewClient(new MyWebViewClient());
+        String url = "https://www.tripadvisor.com/Attractions-g190479-Activities-Oslo_Eastern_Norway.html";
+
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl(url);
+
+    }
+
+    class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
